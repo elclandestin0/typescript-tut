b5ca669c78df850f8c909195b8aa2401ea902c3b
mk <majd.khoury93@gmail.com>
2018-02-10 16:07:31 -0500

    Added .ts file and it's compiled .js file

---
+++ tut.js
@@ -0,0 +1,5 @@
+function greetings(Person) {
+    return "Hello, " + Person;
+}
+var Jane = "Jane";
+document.body.innerHTML = greetings(Jane);
---
+++ tut.ts
@@ -0,0 +1,7 @@
+function greetings(Person: string){
+  return "Hello, " + Person;
+}
+let Jane = "Jane";
+
+
+document.body.innerHTML = greetings(Jane);
